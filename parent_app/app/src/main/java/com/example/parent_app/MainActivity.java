845df8fd36ae815861d24fcf74e4be3dddb18bf9
ChristianParodi <75328764+ChristianParodi@users.noreply.github.com>
2023-05-30 10:56:40 +0200

    Iniziata l'app dei genitori (seriamente)

--- parent_app/app/src/main/java/com/example/parent_app/MainActivity.java
+++ parent_app/app/src/main/java/com/example/parent_app/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -23,51 +25,29 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private static final String IP = "192.168.1.7"; // Da controllare ad ogni avvio
+    private static final String IP = "192.168.151.187"; // Da controllare ad ogni avvio
 
     private static int childId = -1;
-    private Button btnCall;
-    private TextView tvNome;
-    private TextView tvCognome;
-    private TextView tvTipo;
-    private TextView tvData;
+    private TextView tvError;
+    private ScrollView scrollView;
+    private ImageView iwIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCall = findViewById(R.id.btnCall);
-        tvNome = findViewById(R.id.tvNome);
-        tvCognome = findViewById(R.id.tvCognome);
-        tvTipo = findViewById(R.id.tvTipo);
-        tvData = findViewById(R.id.tvData);
+        iwIcon = findViewById(R.id.image_view_icons);
+        tvError = findViewById(R.id.tv_error_scroll);
+        scrollView = findViewById(R.id.scroll_view);
 
         if(childId == -1){
             Intent i = new Intent(this, RequestChildDataActivity.class);
             startActivityForResult(i, RequestChildDataActivity.ACTIVITY_ID);
         }
 
-        btnCall.setOnClickListener(v -> {
-            RequestQueue queue = Volley.newRequestQueue(this);
 
-            JsonArrayRequest stringRequest = new JsonArrayRequest(Request.Method.GET, "http://" + IP + ":3000/select/" + childId, null,
-                    response -> {
-                        try {
-                            for(int i = 0; i < response.length(); i++){
-                                JSONObject current = response.getJSONObject(i);
-                                tvNome.setText(Html.fromHtml("<b>Nome:</b> " + current.getString("nome") + "\n", 0));
-                                tvCognome.setText(Html.fromHtml("<b>Cognome:</b> " + current.getString("cognome") + "\n", 0));
-                                tvTipo.setText(Html.fromHtml("<b>Tipo:</b> " + current.getString("tipo") + "\n", 0));
-                                tvData.setText(Html.fromHtml("<b>Data:</b> " + current.getString("data") + "\n", 0));
-                            }
-                        } catch(JSONException e){
-                            e.printStackTrace();
-                        }
-                    }, error -> tvNome.setText(error.toString())
-            );
-            queue.add(stringRequest);
-        });
+
     }
 
     @Override
@@ -77,6 +57,34 @@ public class MainActivity extends AppCompatActivity {
             if(resultCode == RESULT_OK){
                 assert data != null;
                 childId = data.getIntExtra("child_id", -1);
+
+                RequestQueue queue = Volley.newRequestQueue(this);
+
+                JsonArrayRequest stringRequest = new JsonArrayRequest(Request.Method.GET, "http://" + IP + ":3000/select/" + childId, null,
+                        response -> {
+                            try {
+                                if(response.length() == 0){
+                                    tvError.setText(Html.fromHtml("Il codice inserito non e' valido", 0));
+                                    tvError.setText(response.toString());
+                                    return;
+                                }
+
+                                for(int i = 0; i < response.length(); i++){
+                                    JSONObject current = response.getJSONObject(i);
+                                    TextView record = new TextView(this);
+                                    try {
+                                        record.setText(current.getString("data") + "\t" + current.getString("tipo"));
+                                    } catch (JSONException e){
+                                        tvError.setText(e.toString());
+                                    }
+//                            scrollView.addView(record);
+                                }
+                            } catch(JSONException e){
+                                e.printStackTrace();
+                            }
+                        }, error -> tvError.setText(error.toString())
+                );
+                queue.add(stringRequest);
             }
         }
     }
